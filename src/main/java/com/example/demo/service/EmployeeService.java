2e8fd3b5bee9e120f1b294ff6257b201108c4205
keerthiga <ramarkeerthiga@gmail.com>
2022-11-29 14:53:47 +0530

    first commit

---
+++ src/main/java/com/example/demo/service/EmployeeService.java
@@ -0,0 +1,16 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.Employee;
+
+public interface EmployeeService {
+	List<Employee> getAllEmployees();
+	void saveEmployee(Employee employee);
+	Employee getEmployeeById(long id);
+	void deleteEmployeeById(long id);
+	
+}
